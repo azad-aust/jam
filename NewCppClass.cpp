f156d8dd83c5c751910d31a2491278e2ae7c1f0e
azad <saimon.azad@hotmail.com>
2015-08-23 17:03:07 +0600

    hello html

---
+++ NewCppClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ASUS on 23/08/2015.
+//
+
+#include "NewCppClass.h"
---
+++ NewCppClass.h
@@ -0,0 +1,16 @@
+//
+// Created by ASUS on 23/08/2015.
+//
+
+#ifndef JAM_NEWCPPCLASS_H
+#define JAM_NEWCPPCLASS_H
+
+
+
+class NewCppClass {
+
+};
+
+
+
+#endif //JAM_NEWCPPCLASS_H
